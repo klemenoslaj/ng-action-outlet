309e510a04ccb289da1542121cb2ccf4a9acff90
klemenoslaj <klemenoslaj0@gmail.com>
2018-09-29 20:23:17 +0200

    ci(travis): travis integration
    re-integrate with travis ci

--- projects/core/karma.conf.js
+++ projects/core/karma.conf.js
@@ -25,7 +25,14 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'ChromeHeadless'],
+     // you can define custom flags
+     customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: false
   });
 };
