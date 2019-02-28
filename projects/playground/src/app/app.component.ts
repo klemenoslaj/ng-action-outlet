7db0ad04f8addeb3fb9767ec7b21812371715c96
klemenoslaj <klemenoslaj0@gmail.com>
2019-02-28 14:55:48 +0100

    feat(material): implement material components for ng-action-outlet

--- projects/playground/src/app/app.component.ts
+++ projects/playground/src/app/app.component.ts
@@ -1,23 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-import { ActionOutletFactory, ActionButton } from '@ng-action-outlet/core';
+import { ActionOutletFactory, ActionGroup, ActionButtonEvent } from '@ng-action-outlet/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  action: ActionButton;
-
-  // Defaults
-  icon = 'fa-search';
-  title = 'Search';
+  group: ActionGroup;
 
   constructor(private actionOutlet: ActionOutletFactory) {}
 
   ngOnInit(): void {
-    this.action = this.actionOutlet.createButton({ title: this.title, icon: this.icon });
+    this.group = this.actionOutlet.createGroup();
 
-    this.action.fire$.subscribe(() => alert('button fired'));
+    this.group.createButton().setIcon('home').fire$.subscribe(this.callback);
+    this.group.createButton().setIcon('person').fire$.subscribe(this.callback);
+    this.group.createButton().setIcon('settings').fire$.subscribe(this.callback);
+
+    const childGroup = this.group.createGroup().enableDropdown().setTitle('Dropdown');
+    childGroup.createButton().setTitle('Button 4').fire$.subscribe(this.callback);
+    childGroup.createButton().setTitle('Button 5').fire$.subscribe(this.callback);
+    childGroup.createButton().setTitle('Button 6').fire$.subscribe(this.callback);
+
+    const childGroup2 = childGroup.createGroup().enableDropdown().setTitle('Dropdown 2');
+    childGroup2.createButton().setTitle('Button 4').fire$.subscribe(this.callback);
+    childGroup2.createButton().setTitle('Button 5').fire$.subscribe(this.callback);
+    childGroup2.createButton().setTitle('Button 6').fire$.subscribe(this.callback);
+
+    const childGroup3 = childGroup2.createGroup().enableDropdown().setTitle('Dropdown 3');
+    childGroup3.createButton().setTitle('Button 7').fire$.subscribe(this.callback);
   }
+
+  callback = ({ action }: ActionButtonEvent) => console.log('Clicked:', action.getTitle() || action.getIcon());
 }
--- projects/playground/src/app/button/button.component.ts
+++ projects/playground/src/app/button/button.component.ts
@@ -1,18 +1,70 @@
-import { Component, OnInit, HostBinding } from '@angular/core';
-import { ActionButton, ActionAbstractComponentImpl } from '@ng-action-outlet/core';
+import { Component, OnInit, HostBinding, Input } from '@angular/core';
+import {
+  ActionButton,
+  ActionAbstractComponentImpl
+} from '@ng-action-outlet/core';
 
 @Component({
-  selector: 'app-button',
-  templateUrl: './button.component.html',
-  styleUrls: ['./button.component.css']
+  template: `
+    <ng-container *ngIf="action && isButton(); then button else menuItem"></ng-container>
+
+    <ng-template #button>
+      <ng-container *ngIf="action.title$ | async; then titleButton else iconButton"></ng-container>
+    </ng-template>
+
+    <ng-template #titleButton>
+      <button
+        type="button"
+        mat-button
+        (click)="action.trigger()"
+        [disabled]="action.disabled$ | async">
+        <ng-container [ngTemplateOutlet]="content"></ng-container>
+      </button>
+    </ng-template>
+
+    <ng-template #iconButton>
+      <button
+        type="button"
+        mat-icon-button
+        (click)="action.trigger()"
+        [disabled]="action.disabled$ | async">
+        <ng-container [ngTemplateOutlet]="content"></ng-container>
+      </button>
+    </ng-template>
+
+    <ng-template #menuItem>
+      <button
+        type="button"
+        mat-menu-item
+        (click)="action.trigger()"
+        [disabled]="action.disabled$ | async">
+        <ng-container [ngTemplateOutlet]="content"></ng-container>
+      </button>
+    </ng-template>
+
+    <ng-template #content>
+      <mat-icon *ngIf="action.icon$ | async; let icon">{{ icon }}</mat-icon>
+      {{ action.title$ | async }}
+    </ng-template>
+  `,
+  styleUrls: ['./button.component.scss']
 })
 export class ButtonComponent implements OnInit, ActionAbstractComponentImpl<ActionButton> {
+  @Input()
   readonly action: ActionButton;
 
   @HostBinding()
   hidden: boolean;
 
   ngOnInit() {
-    this.action.visible$.subscribe(visibility => this.hidden = !visibility);
+    this.action.visible$.subscribe(visibility => (this.hidden = !visibility));
+  }
+
+  isButton() {
+    return !this.action.getParent() || !this.action.getParent().isDropdown();
+  }
+
+  isMenuItem() {
+    return this.action.getParent() && this.action.getParent().isDropdown();
   }
 }
