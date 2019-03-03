c5e224429509b94a9d7b0be7a11e569f3af8adb6
klemenoslaj <klemenoslaj0@gmail.com>
2019-03-03 21:57:47 +0100

    fix(material): render group button as a button not menu-item

--- projects/material/src/lib/common.ts
+++ projects/material/src/lib/common.ts
@@ -1,5 +1,5 @@
 import { AnyAction, ActionGroup } from '@ng-action-outlet/core';
 
 export const isMenuItem = (action: AnyAction) => {
-  return action && (action instanceof ActionGroup && action.isDropdown() || isMenuItem(action.getParent()));
+    return action && (action instanceof ActionGroup && action.isDropdown() || isMenuItem(action.getParent()));
 };
