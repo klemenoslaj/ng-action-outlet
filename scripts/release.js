72dfc28e25cbddfeafb8812b8359205116d8d000
Klemen Oslaj <klemenoslaj0@gmail.com>
2019-04-20 18:05:07 +0200

    fix: verify travis npm publis process

--- scripts/release.js
+++ scripts/release.js
@@ -3,26 +3,41 @@ const shell = require('shelljs');
 
 async function prepare({ packages }, context) {
     const PLACEHOLDER_VERSION = '0.0.0-development';
-    const { nextRelease: { version }, logger } = context;
+    const { nextRelease: { version }, logger, env } = context;
 
     shell
         .ls(`${packages}/*/package.json`)
         .forEach(file => {
-            shell.exec(`npm version ${version} --no-git-tag-version`, { cwd: dirname(file) });
+            shell.exec(`npm version ${version} --no-git-tag-version`, { cwd: dirname(file), env });
             shell.sed('-i', PLACEHOLDER_VERSION, version, file);
             logger.log(`Write version ${version} to package.json in ${file}`);
         });
 }
 
 async function publish({ packages }, context) {
-    const { logger } = context;
+    const { logger, env } = context;
+
+    if (!env.NPM_TOKEN) {
+        throw new Error('NPM_TOKEN environment');
+    }
+
+    shell.ShellString(`//registry.npmjs.org/:_authToken=${env.NPM_TOKEN}`).to('~/.npmrc');
 
     shell
         .ls(`${packages}/*/package.json`)
         .map(file => ({ pkgPath: dirname(file), package: JSON.parse(shell.cat(file)) }))
-        .forEach(({ pkgPath: packagePath, package }) => {
-            shell.exec(`npm publish ${packagePath} --access public`, { cwd: packagePath });
-            logger.log(`Published ${package.name}@${package.version}.`);
+        .forEach(({ pkgPath: cwd, package }) => {
+            const config = `${cwd}/.npmrc`;
+
+            shell.ShellString(`//registry.npmjs.org/:_authToken=${env.NPM_TOKEN}`).to(config);
+            logger.log(`Wrote NPM_TOKEN to ${config}`);
+
+
+            if (shell.exec(`npm publish --access public`, { cwd, env }).code === 0) {
+                logger.log(`Published ${package.name}@${package.version}.`);
+            } else {
+                throw new Error(`The release of ${package.name} failed.`);
+            }
         });
 }
 
