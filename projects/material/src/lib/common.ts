08de7ec4321fef7dd25213369981c47279ac0dee
klemenoslaj <klemenoslaj0@gmail.com>
2019-05-08 16:39:13 +0200

    fix(material): make the library run in strict TypeScript configuration

--- projects/material/src/lib/common.ts
+++ projects/material/src/lib/common.ts
@@ -1,5 +1,5 @@
-import { AnyAction, ActionGroup } from '@ng-action-outlet/core';
+import { AnyAction, ActionGroup, ActionAbstract } from '@ng-action-outlet/core';
 
-export const isMenuItem = (action: AnyAction) => {
-    return action && (action instanceof ActionGroup && action.isDropdown() || isMenuItem(action.getParent()));
-};
+export function isMenuItem(action?: AnyAction): boolean {
+    return action instanceof ActionAbstract && (action instanceof ActionGroup && action.isDropdown() || isMenuItem(action.getParent()));
+}
